f24f7b82c8b8fea5918a2d999fd832ad1200f4d7
Glauco Custódio <glauco.custodio@gmail.com>
2012-06-29 20:14:58 -0300

    Project creation and MySQL database configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TccIasp::Application.config.secret_token = '948606c0d5e3735e1e0658f21e4c70c839f41c01c47136fc183e7245bfb7ce4fa666e8be009e610758a3a2cdf8f494d947fc27652de69cf02d86f520b30a3f21'
