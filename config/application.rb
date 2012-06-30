9de7ed4edcbac89be36f0f49b6e00cf67803e239
Glauco Custódio <glauco.custodio@gmail.com>
2012-06-29 22:36:41 -0300

    Inclusion of yml from pt-BR file for Devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,6 @@ module TccIasp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.i18n.default_locale = :sv
   end
 end
